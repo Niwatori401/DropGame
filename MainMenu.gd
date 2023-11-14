67c28aaaa0f8a773aa59ca332d7258de67badfe3
Niwatori401 <cjackson@firemail.cc>
2023-11-14 16:05:41 +0900

    Improve main menu

--- MainMenu.gd
+++ MainMenu.gd
@@ -2,6 +2,13 @@ extends Node
 
 var should_fade_out = false;
 
+var title_growing = true;
+var MINIMUM_TITLE_SIZE = 0.98;
+var MAXIMUM_TITLE_SIZE = 1.02;
+var current_title_size = 1;
+var TITLE_GROWTH_RATE = 0.01;
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MainMenuSelectionScreen/DarkScreen.show();
@@ -10,6 +17,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$MainMenuSelectionScreen/Background_1.rotation += delta * 0.1
+	$MainMenuSelectionScreen/Background_2.rotation -= delta * 0.2
+	
+	if (title_growing):
+		current_title_size += TITLE_GROWTH_RATE * delta;
+		if (current_title_size >= MAXIMUM_TITLE_SIZE):
+			title_growing = false;
+	else:
+		current_title_size -= TITLE_GROWTH_RATE * delta;
+		if (current_title_size <= MINIMUM_TITLE_SIZE):
+			title_growing = true;
+	
+	$MainMenuSelectionScreen/GameTitle.scale = Vector2(current_title_size, current_title_size)
+	
 	if should_fade_out:
 		if $MainMenuSelectionScreen/DarkScreen.modulate.a < 0:
 			$MainMenuSelectionScreen/DarkScreen.modulate.a = 0;
--- options_menu.gd
+++ options_menu.gd
@@ -22,3 +22,21 @@ func _on_back_button_pressed():
 	should_fade_out = true;
 	get_tree().create_timer(2).timeout.connect(func(): get_tree().change_scene_to_file("res://main_menu.tscn"))
 	
+
+
+func _on_music_slider_value_changed(value):
+	if (value == 0):
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true);
+	else:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), false);
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), lerp(-30, 15, value / 100));
+
+
+func _on_sfx_slider_value_changed(value):
+	if (value == 0):
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"), true);
+	else:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"), false);
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), lerp(-30, 15, value / 100));
+
+
